fe2e0bef740be3610bd990d0395ae36e2ca55c67
Will Templeton <templetw@bc.edu>
2020-01-17 19:25:37 -0500

    first commit

--- TestMessageApp/ViewController.swift
+++ TestMessageApp/ViewController.swift
@@ -9,12 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var textBox: UITextView!
+    
+    @IBOutlet weak var excuseBox: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("viewDidLoad")
     }
 
-
+    @IBAction func messageButtonPressed(_ sender: UIButton) {
+        print("message button has been pressed!")
+        messageLabel.text = "Obi-Wan Kenobi"
+    }
+    @IBAction func resetButtonPressed(_ sender: UIButton) {
+        print("reset button has been pressed!")
+        messageLabel.text = "Hello There!"
+    }
+    @IBAction func yesButtonPressed(_ sender: UIButton) {
+        print("yes button pressed")
+        textBox.text = "Goooooood"
+        textBox.textAlignment = .center
+        
+    }
+    @IBAction func noButtonPressed(_ sender: UIButton) {
+        print("no button pressed")
+        textBox.text = "It's not a story the Jedi would tell you..."
+        textBox.textAlignment = .center
+    }
+    @IBAction func resetallButtonPressed(_ sender: UIButton) {
+        print("reset all button pressed")
+        textBox.text = "Have you ever heard the tragedy of Darth Plageuis the Wise?"
+        messageLabel.text = "Hello There!"
+    }
+    @IBAction func genExcusePressed(_ sender: UIButton) {
+        print("generate excuse button pressed")
+        excuseBox.text = "I need a better excuse"
+    }
+    
+    
+    
 }
 
